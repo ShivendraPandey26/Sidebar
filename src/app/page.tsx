657a46539b11028edbae45ae839110fe2305d531
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-08-02 03:07:40 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,8 @@ import Dashboard from "@/components/Dashboard";
 const Home = () => {
   const links = [
     {
-      label: "Dashboard",
-      href: "#",
+      label: "Home",
+      href: "/",
       icon: (
         <IconBrandTabler className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
@@ -47,24 +47,40 @@ const Home = () => {
   ];
 
   const [open, setOpen] = useState(false);
+  const [darkmode, setDarkmode] = useState(true);
+
+  const handleDarkmode = () => {
+    setDarkmode(!darkmode);
+  }
 
   return (
     <div
       className={cn(
-        "rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 border border-neutral-200 dark:border-neutral-700 overflow-hidden",
-        "h-screen" // for your use case, use `h-screen` instead of `h-[60vh]`
+        `rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+        h-screen  ${darkmode && "dark"}`
       )}
     >
       <Sidebar open={open} setOpen={setOpen}>
         <SidebarBody className="justify-between gap-10">
           <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
             {open ? <Logo /> : <LogoIcon />}
+
+            {/* middle */}
             <div className="mt-8 flex flex-col gap-2">
               {links.map((link, idx) => (
                 <SidebarLink key={idx} link={link} />
               ))}
             </div>
           </div>
+
+          <hr />
+          <div className="text-blue-500">
+            <button onClick={handleDarkmode}>
+              {darkmode ?  "Light Mode" : "Dark Mode" }
+            </button>
+          </div>
+
+          {/* lower portion */}
           <div>
             <SidebarLink
               link={{
