23d73dcf0cee388ddcb11f8beff072140b014dbe
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-08-02 15:01:53 +0530

    Update Dashboard.tsx

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,7 +4,7 @@ import HomePage from "./HomePage";
 const Dashboard = () => {
     return (
       <div className="flex flex-1">
-        <div className="p-2 md:p-10 border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full ">
+        <div className=" p-2 md:p-10 border-8 border-neutral-100 dark:border-neutral-800 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full ">
           <HomePage />
         </div>
       </div>
