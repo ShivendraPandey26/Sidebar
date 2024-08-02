e1c60851caa429bb0bb7412a44e66dfd1c963e02
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-08-02 15:01:59 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ const Home = () => {
   return (
     <div
       className={cn(
-        `rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1 border border-neutral-200 dark:border-neutral-700 overflow-hidden",
+        `rounded-md flex flex-col md:flex-row dark:bg-gray-100 bg-neutral-800 w-full flex-1 overflow-y-scroll
         h-screen  ${darkmode && "dark"}`
       )}
     >
