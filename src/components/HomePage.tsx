752b853d19ccd515a2ffe7002ee6f80cf5f5c28b
ShivendraPandey26 <aviralpandey7974@gmail.com>
2024-08-02 15:01:51 +0530

    Update HomePage.tsx

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -4,27 +4,31 @@ import TechIcons from "./TechIcons";
 
 function HomePage() {
   return (
-    <div className="p-10">
-      <div className="dark:text-white text-black">
-        <span className="text-5xl">👋</span>
-        <h1 className="font-mono text-2xl lg:text-4xl">
-          Hello there! I&apos;m Shivendra Kumar Pandey
-        </h1>
-      </div>
+    <>
+      <div className="container p-10">
+        <div className="dark:text-white text-black">
+          <h1 className="font-mono text-2xl lg:text-4xl" data-aos="zoom-in-up">
+            <span className="h-20">👋</span>
+            <br /> Hello there! I&apos;m Shivendra Kumar Pandey
+          </h1>
+        </div>
 
-      <div className="dark:text-white text-black">
-        <p className="text-[#737373] py-5">
-          I’m a Full-Stack Developer dedicated to building impactful products
-          and web applications that make a difference. With a passion for
-          creating solutions that reach and benefit millions, I bring a blend of
-          creativity and technical expertise to every project. <br /> <br /> Explore my
-          work and connect with me to collaborate on creating something
-          extraordinary!
-        </p>
+        <div className="dark:text-white text-black" data-aos="zoom-in-up">
+          <p className="text-[#737373] py-5">
+            I’m a Full-Stack Developer dedicated to building impactful products
+            and web applications that make a difference. With a passion for
+            creating solutions that reach and benefit millions, I bring a blend
+            of creativity and technical expertise to every project. <br />{" "}
+            <br />
+            Explore my work and connect with me to collaborate on creating
+            something extraordinary!
+          </p>
+        </div>
       </div>
-
-      <TechIcons /> 
-    </div>
+      <div className="p-3 lg:px-20">
+        <TechIcons />
+      </div>
+    </>
   );
 }
 
